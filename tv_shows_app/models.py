12611330f967a3f66c8a60f3ebe312f3a3356fd5
AndrewCWheeler <achristopherwheeler@gmail.com>
2020-05-16 19:11:31 -0700

    second commit

--- tv_shows_app/models.py
+++ tv_shows_app/models.py
@@ -1,10 +1,26 @@
+from datetime import date
 from django.db import models
+
+class ShowManager(models.Manager):
+    def show_validator(self, postData):
+        errors = {}
+
+        if len(postData['title']) < 2:
+            errors['title'] = "Title must be at least 2 characters."
+        if len(postData['network']) < 3:
+            errors['network'] = "Network must be at least 3 characters."
+        if len(postData['desc']) < 10 and len(postData['desc']) >= 1:
+            errors['desc'] = "Description must be at least 10 characters."
+        if postData['date'] > str(date.today()):
+            errors['date'] = "Date must not be in the future."
+        return errors
 
 class Show(models.Model):
     title = models.CharField(max_length=255)
     network = models.CharField(max_length=255)
     date = models.CharField(max_length=45)
-    desc = models.TextField()
+    desc = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = ShowManager()
 
--- tv_shows_app/views.py
+++ tv_shows_app/views.py
@@ -1,10 +1,13 @@
+from datetime import date
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import Show
 
-
 def shows(request):
+    
     context = {
         'all_shows' : Show.objects.all(),
+        
     }
     return render(request, 'all_shows.html', context)
 
@@ -12,17 +15,24 @@ def new_show_form(request):
     return render(request, 'new_show.html')
 
 def create_show(request):
-    title = request.POST['title']
-    network = request.POST['network']
-    date = request.POST['date']
-    desc = request.POST['desc']
-    new_show = Show.objects.create(
-        title=title,
-        network=network,
-        date=date,
-        desc=desc,
-    )
-    show_id = new_show.id
+    errors = Show.objects.show_validator(request.POST)
+
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/shows/new')
+    else:
+        title = request.POST['title']
+        network = request.POST['network']
+        date = request.POST['date']
+        desc = request.POST['desc']
+        new_show = Show.objects.create(
+            title=title,
+            network=network,
+            date=date,
+            desc=desc,
+        )
+        show_id = new_show.id
     return redirect(f'/shows/{show_id}')
 
 def view_show(request, show_id):
@@ -50,17 +60,23 @@ def edit_show_form(request, show_id):
 
 def edit_show(request):
     show_id = request.POST['show_id']
-    title = request.POST['title']
-    network = request.POST['network']
-    date = request.POST['date']
-    desc = request.POST['desc']
-    e = Show.objects.get(id=show_id)
-    e.title = title
-    e.network = network
-    e.date = date
-    e.desc = desc
-    e.save()
-    return redirect(f'/shows/{show_id}')
+    errors = Show.objects.show_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect(f'/shows/{show_id}/edit')
+    else:
+        title = request.POST['title']
+        network = request.POST['network']
+        date = request.POST['date']
+        desc = request.POST['desc']
+        e = Show.objects.get(id=show_id)
+        e.title = title
+        e.network = network
+        e.date = date
+        e.desc = desc
+        e.save()
+        return redirect(f'/shows/{show_id}')
 
 def delete_show(request, show_id):
     d = Show.objects.get(id=show_id)
